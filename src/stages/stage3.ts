de0b1d6dbaceb145afde9a1822df8aad13771b0d
alice <aliceisjustplaying@gmail.com>
2024-10-18 19:21:09 +0100

    asgh

--- src/stages/stage3.ts
+++ src/stages/stage3.ts
@@ -183,6 +183,7 @@ export async function processDidsAndFetchData(dids: DidAndPds[]): Promise<void> 
           );
 
           context.successfulRequests++;
+          didsSuccessfulTotal.inc();
 
           // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
           await processStream(res.data, did, context);
